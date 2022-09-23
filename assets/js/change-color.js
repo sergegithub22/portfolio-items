f1ef6edfa31e06ef5789b0ec724d52589b2beef5
Sergei <112075321+sergegithub22@users.noreply.github.com>
2022-09-23 12:07:19 +0300

    Update change-color.js
    1207

--- assets/js/change-color.js
+++ assets/js/change-color.js
@@ -147,38 +147,3 @@ for (var i = buttons.length - 1; i >= 0; i--) {
     buttons[i].addEventListener("click", changeBgColor);
     
 }
-
-
-
-//console.log(links);
-
-
-
-
-//function showHideSiteLogo() {
-//
-//  if(logo.classList.contains("visually-hidden")){
-//  logo.classList.remove("visually-hidden");
-//  link[0].textContent = "Hide Site Logo";
-//  window.alert("Now u will see the logo of this site template. For hide it - click, please, on 'Hide Site Logo' in 'My Account' menu");
-//    
-//}  
-//  else{
-//    logo.classList.add("visually-hidden");
-//    link[0].textContent = "Show site Logo";
-//  }
-//  console.log([link]); 
-//  
-//}
-//var link = document.querySelectorAll(".show_site_logo");
-//var logo = document.querySelector(".logo");
-//
-//
-//for (var i = link.length - 1; i >= 0; i--) {
-//	link[i].addEventListener("click", showHideSiteLogo);
-//}
-//console.log(logo.style);
-
-
-
-
