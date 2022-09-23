4dd31d845f06e1be28acbf0e4282cd5b772681c3
Sergei <112075321+sergegithub22@users.noreply.github.com>
2022-09-23 12:05:58 +0300

    230922
    1205

--- assets/js/change-color.js
+++ assets/js/change-color.js
@@ -14,19 +14,49 @@ var targetColor = evt.currentTarget.title;
 
   if (targetColor === "Dark gray"){
     bg.style.backgroundColor = "#1a1a1a";
+    bg.style.transition = "2.5s";
+    for (var e = elementItem.length - 1; e >= 0; e--){
+      elementItem[e].style.backgroundColor = "#333333";
+      elementItem[e].style.color = "lightgray";
+    }
+    for (var h = elementHeader.length - 1; h >= 0; h--){
+      elementHeader[h].style.backgroundColor = "#333333";
+    }
+    for (var d = elementDesc.length - 1; d >= 0; d--){
+      elementDesc[d].style.backgroundColor = "#262626";
+      elementDesc[d].style.color = "inherit";
+    }
+    for (var c = credits.length - 1; c >= 0; c--){
+      credits[c].style.color = "white";
+    }
+    for (var cl = creditsLink.length - 1; cl >= 0; cl--) {
+      creditsLink[cl].style.color = "white";
+      creditsLink[cl].addEventListener("mouseover", (event) => {
+      event.target.style.color = "inherit";
+      });
+      creditsLink[cl].addEventListener("mouseout", (event) => {
+      event.target.style.color = "inherit";
+      });
+    }
+    for (var l = elementLinks.length - 1; l >= 0; l--){
+      elementLinks[l].style.backgroundColor = "#333333";
+    }
     for (var x = links.length - 1; x >= 0; x--) {
+      links[x].style.color = "white";
       links[x].addEventListener("mouseenter", (event) => {
       event.target.style.backgroundColor = "#404040";
+      event.target.style.color = "white";
       });
       links[x].addEventListener("mouseout", (event) => {
       event.target.style.backgroundColor = "";
+      event.target.style.color = "white";
       });
     }
   }
 //  Yellow
   else if (targetColor === "Yellow"){
     bg.style.backgroundColor = "#997a00";
-    bg.style.transition = "2.5s"
+    bg.style.transition = "2.5s";
     for (var e = elementItem.length - 1; e >= 0; e--){
       elementItem[e].style.backgroundColor = "#cccc00";
       elementItem[e].style.color = "black";
@@ -68,7 +98,7 @@ var targetColor = evt.currentTarget.title;
 //  Pink
    else if (targetColor === "Pink"){
     bg.style.backgroundColor = "#330033";
-    bg.style.transition = "2.5s"
+    bg.style.transition = "2.5s";
     for (var e = elementItem.length - 1; e >= 0; e--){
       elementItem[e].style.backgroundColor = "#990099";
       elementItem[e].style.color = "white";
